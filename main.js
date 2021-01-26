f5013e4c156bf963601244cd2dcba41e160ada50
Jaruwat Pohong <jara7897@gmail.com>
2021-01-27 02:37:44 +0700

    Implement scroll function & Filter file extension

--- main.js
+++ main.js
@@ -4,16 +4,17 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 const sanitize = require("sanitize-filename");
-const Crawler = require("crawler")
+const puppeteer = require('puppeteer');
 const fs = require('fs'),
     path = require("path"),
     URL = require("url"),
     rimraf = require("rimraf");
-
+const cliProgress = require('cli-progress');
 const fetch = require('node-fetch');
+//const scrollPageToBottom = require('puppeteer-autoscroll-down')
 
 let crrDomain,
-    crrDomainStr = ''
+    imgExt = ['jpg', 'png', 'jpeg', 'gif', 'bmp', 'tif', 'tiff', 'webp', 'svg']
 
 function download(url, localPath, fn) {
     return new Promise(async resolve => {
@@ -22,12 +23,15 @@ function download(url, localPath, fn) {
             if(parsed.hostname === null)
                 url = 'http://' + crrDomain.hostname + (url.charAt(0) != "/" ? "/" : "") + url
             const response = await fetch(url);
-            const buffer = await response.buffer();
-            let fileName = localPath + ' - ' + sanitize(path.basename(parsed.pathname))
-            fs.writeFileSync(fileName, buffer);
-            console.log('Saved to: ' + fileName)   
+            let ext = response.headers.get('content-type').split('/')
+            if(ext[0] == "image" && imgExt.includes(ext[1])) {
+                const buffer = await response.buffer();
+                let fileName = localPath + '.' + ext[1] // + ' - ' + sanitize(path.basename(parsed.pathname))
+                fs.writeFileSync(fileName, buffer);
+                console.log('Saved to: ' + fileName)   
+            }
         } catch (error) {
-            console.log('Failed to save this url: ' + url)
+            console.log(error.message, 'Failed to save this url: ' + url)
         }
         if(fn) fn()
         resolve()
@@ -41,46 +45,136 @@ function pad(num, len) {
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-const c = new Crawler({
-    callback: async function(error, res, done) {
-        if (error) {
-            console.error(`Cannot request to ${crrDomainStr}. Because ${error.message}\n`)
-            prompt()
-        } else {
-            if (!fs.existsSync('images'))
-                fs.mkdirSync('images');
-            const images = res.$('img')
-            const title = res.$('title')
-            console.log('Start download images from: ' + title.text())
-            let startTime = Date.now()
-            let pathTar = 'images/' + sanitize(title.text())
-            if (!fs.existsSync(pathTar))
-                fs.mkdirSync(pathTar);
-            else {
-                rimraf.sync(pathTar);
-                fs.mkdirSync(pathTar);
-            }
-            let count = 0
-            for (let index = 0; index < images.length; index++) {
-                download(images[index].attribs.src, `${pathTar}/${pad(index, 4)}`, ()=> {
-                    count++;
-                })
-            }
-            while (count < images.length) {
-                await sleep(10)
-            }
-            let finishTime = Date.now() - startTime
-            console.log(`Finished at ${finishTime} ms, Saved in ${pathTar}!\n`)
-            prompt()
-        }
+
+async function fetchingImages(images, title) {
+    if (!fs.existsSync('images'))
+        fs.mkdirSync('images');
+    console.log('Start download images from: ' + title)
+    let startTime = Date.now()
+    let pathTar = 'images/' + sanitize(title)
+    if (!fs.existsSync(pathTar))
+        fs.mkdirSync(pathTar);
+    else {
+        rimraf.sync(pathTar);
+        fs.mkdirSync(pathTar);
     }
-})
+    let count = 0
+    for (let index = 0; index < images.length; index++) {
+        download(images[index], `${pathTar}/${pad(index, 4)}`, ()=> {
+            count++;
+        })
+    }
+    while (count < images.length) {
+        await sleep(10)
+    }
+    let finishTime = Date.now() - startTime
+    console.log(`Finished at ${finishTime} ms, Saved in ${pathTar}!\n`)
+    prompt()
+}
+
+let lazyLoadOn = false;
 
 function prompt() {
     rl.question("Website url: ", function(link) {
         crrDomainStr = link
         crrDomain = URL.parse(link)
-        c.queue(link)
+        //c.queue(link)
+
+        puppeteer.launch().then(async browser => {
+            const page = await browser.newPage();
+            const bar1 = new cliProgress.SingleBar({
+                format: `Scrolling [{bar}] {percentage}% | ETA: {eta}s `
+            }, cliProgress.Presets.legacy);
+            page.on('console', consoleObj => {
+                let progress = consoleObj.text().split("/")
+                //console.log(progress)
+                bar1.update(parseInt(progress[0]));
+            });
+            await page.goto(link);
+            await page.setViewport({
+                width: 1200,
+                height: 2000
+            });
+            console.log('Scanning for ' + await page.title() + '.')
+            //await page.screenshot({path: 'screenshot.png'});
+            if(lazyLoadOn) {
+                console.log('Starting 1st Scroll... | Step 1000 | Delay 300')
+                bar1.start(100, 0);
+                await scrollPageToBottom(page, 1000, 300)
+                await page.evaluate(_ => {
+                    window.scrollBy(0, -window.scrollY)
+                });
+                bar1.stop();
+                console.log('Starting 2nd Scroll... | Step 1000 | Delay 300')
+                bar1.start(100, 0);
+                await scrollPageToBottom(page, 1000, 300)
+                await page.evaluate(_ => {
+                    window.scrollBy(0, -window.scrollY)
+                });
+                bar1.stop();
+                console.log('Starting 3rd Scroll... | Step 900 | Delay 50')
+                bar1.start(100, 0);
+                await scrollPageToBottom(page, 900, 50)
+                await page.evaluate(_ => {
+                    window.scrollBy(0, -window.scrollY)
+                });
+                bar1.stop();
+                console.log('Starting 4th Scroll... | Step 500 | Delay 50')
+                bar1.start(100, 0);
+                await scrollPageToBottom(page, 500, 50)
+                await page.evaluate(_ => {
+                    window.scrollBy(0, -window.scrollY)
+                });
+                bar1.stop();
+            }
+            await page.screenshot({path: 'screenshot.png'});
+            console.log('Loading data...')
+            await page.waitForTimeout(1000)
+            const imgs = await page.$$eval('img[src]', imgs => imgs.map(img => img.getAttribute('src')));
+            fetchingImages(imgs, await page.title())
+
+            await browser.close();
+        });
     });
 }
-prompt()
+
+
+rl.question("Do you want to enable lazy load?\nType `yes`|`y` to enable, or leave it blank to skip: ", function(ans) {
+    if(ans.toLowerCase() == "yes" || ans.toLowerCase() == "y") lazyLoadOn = true
+    console.log((lazyLoadOn ? "Lazy load is now enabled!" : "Lazy load is now disabled.") + '\n')
+    prompt()
+})
+
+async function scrollPageToBottom(page, scrollStep = 250, scrollDelay = 70) {
+    const lastPosition = await page.evaluate(
+        async (step, delay) => {
+            const getScrollHeight = (element) => {
+            if (!element) return 0
+    
+                const { scrollHeight, offsetHeight, clientHeight } = element
+                return Math.max(scrollHeight, offsetHeight, clientHeight)
+            }
+            
+            const position = await new Promise((resolve) => {
+                let count = 0
+                const intervalId = setInterval(() => {
+                    const { body } = document
+                    const availableScrollHeight = getScrollHeight(body)
+                    window.scrollBy(0, step)
+                    count += step
+                    //console.log(`Scrolling... ${((count / availableScrollHeight) * 100).toFixed(2)}%`)
+                    console.log(Math.round((count / availableScrollHeight) * 100) + '/' + 100)
+                    if (count >= availableScrollHeight) {
+                        clearInterval(intervalId)
+                        resolve(count)
+                    }
+                }, delay)
+            })
+    
+            return position
+        },
+        scrollStep,
+        scrollDelay
+    )
+    return lastPosition
+}
